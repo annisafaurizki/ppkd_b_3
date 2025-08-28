97d1cdea9a380af3f880389107be7076f577452a
annisafaurizki <annisafaurizki193@gmail.com>
2025-08-28 12:34:29 +0700

    register API

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:ppkd_b_3/tugas_tugas/pokemon/view.dart/get_pokemon_api.dart';
+import 'package:ppkd_b_3/tugas_tugas/RegisterAPI/View/login_api_screen.dart';
 
 void main() {
   initializeDateFormatting("id_ID");
@@ -34,7 +34,8 @@ class MyApp extends StatelessWidget {
         fontFamily: "Gilroy",
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Pokemon23Get(),
+
+      home: LoginAPIScreen(),
     );
   }
 }
---
+++ lib/tugas_tugas/RegisterAPI/API/register.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:ppkd_b_3/share_preferences/share_preferences.dart';
+import 'package:ppkd_b_3/tugas_tugas/RegisterAPI/API/endpoint/endpoint.dart';
+import 'package:ppkd_b_3/tugas_tugas/RegisterAPI/Model/get_user_model.dart';
+import 'package:ppkd_b_3/tugas_tugas/RegisterAPI/Model/register_model.dart';
+
+class AuthenticationAPI {
+  static Future<RegisterUserModel> registerUser({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    final url = Uri.parse(Endpoint.register);
+    final response = await http.post(
+      url,
+      body: {"name": name, "email": email, "password": password},
+    );
+    if (response.statusCode == 200) {
+      return RegisterUserModel.fromJson(json.decode(response.body));
+    } else {
+      final error = json.decode(response.body);
+      throw Exception(error["message"] ?? "Register gagal");
+    }
+  }
+
+  static Future<RegisterUserModel> loginUser({
+    required String email,
+    required String password,
+  }) async {
+    final url = Uri.parse(Endpoint.register);
+    final response = await http.post(
+      url,
+      body: {"email": email, "password": password},
+    );
+    if (response.statusCode == 200) {
+      return RegisterUserModel.fromJson(json.decode(response.body));
+    } else {
+      final error = json.decode(response.body);
+      throw Exception(error["message"] ?? "Register gagal");
+    }
+  }
+
+  static Future<GetUserModel> updateUser({required String name}) async {
+    final url = Uri.parse(Endpoint.profile);
+    final response = await http.post(
+      url,
+      body: {"name": name},
+      headers: {"Accept": "application/json"},
+    );
+    if (response.statusCode == 200) {
+      return GetUserModel.fromJson(json.decode(response.body));
+    } else {
+      final error = json.decode(response.body);
+      throw Exception(error["message"] ?? "Register gagal");
+    }
+  }
+
+  static Future<GetUserModel> getProfile() async {
+    final url = Uri.parse(Endpoint.profile);
+    final token = await PreferenceHandler.getToken();
+    final response = await http.get(
+      url,
+      headers: {"Accept": "application/json", "Authorization": token},
+    );
+    if (response.statusCode == 200) {
+      return GetUserModel.fromJson(json.decode(response.body));
+    } else {
+      final error = json.decode(response.body);
+      throw Exception(error["message"] ?? "Register gagal");
+    }
+  }
+}
